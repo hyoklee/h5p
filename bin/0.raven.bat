f616f59e8b875278c0a2c119a32e1c0d3830f81b
H. Joe Lee <hyoklee@hdfgroup.org>
2024-04-18 16:03:21 -0500

    ci(oneAPI): change site name

---
+++ bin/0.raven.bat
@@ -0,0 +1,2 @@
+REM "c:\Program Files (x86)\Intel\oneAPI\setvars.bat"
+"D:\one\2024.1\oneapi-vars.bat"
--- bin/i.raven.bat
+++ bin/i.raven.bat
@@ -1,8 +1,11 @@
 set VS2022INSTALLDIR=D:\VS\
 set PATH=D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\;D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;%PATH%
+set LIB=D:\vs\vc\Tools\MSVC\14.39.33519\lib\x64;%LIB%
+set LIBPATH=D:\vs\vc\Tools\MSVC\14.39.33519\lib\x64;%LIBPATH%
+set INCLUDE=D:\VS\VC\Tools\MSVC\14.39.33519\include;%INCLUDE%
 rmdir /q /s intel
 mkdir intel
 cd intel
-D:\one\setvars
-D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake -G Ninja -DBUILDNAME=icx.p -DCMAKE_C_COMPILER="D:/one/mpi/latest/bin/mpicc.bat" -DCMAKE_CXX_COMPILER="D:/one/mpi/latest/bin/mpicc.bat" -DHDF5_ENABLE_PARALLEL:BOOL=ON  -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SUBFILING_VFD:BOOL=OFF -DMPIEXEC_EXECUTABLE:FILEPATH="D:/one/mpi/latest/bin/mpiexec.exe" -DMPI_C_HEADER_DIR:PATH="D:/one/mpi/latest/include" -DCTEST_DROP_SITE_INIT=my.cdash.org -DSITE=win10 ..
-D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\ctest -D Experimental
+D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake -G Ninja -DBUILDNAME="ninja/icx/+p" -DCMAKE_C_COMPILER="D:/one/compiler/latest/bin/icx.exe" -DCMAKE_CXX_COMPILER="D:/one/compiler/latest/bin/icpx.exe" -DCMAKE_Fortran_COMPILER="D:/one/compiler/latest/bin/ifx.exe" -DCMAKE_CXX_COMPILER="D:/one/compiler/latest/bin/icpx.exe"  -DHDF5_BUILD_FORTRAN:BOOL=OFF  -DHDF5_ENABLE_PARALLEL:BOOL=ON  -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SUBFILING_VFD:BOOL=OFF -DMPIEXEC_EXECUTABLE:FILEPATH="D:/one/mpi/latest/bin/mpiexec.exe" -DMPI_C_HEADER_DIR:PATH="D:/one/mpi/latest/include" -DCTEST_DROP_SITE_INIT=my.cdash.org -DSITE=win-10 ..
+D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\ctest -C Release -D Continuous -E ".*t_pflush.*"
+cd ..
