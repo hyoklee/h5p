a780e670f8a2635dd2af3c506b0eac7ae3329b1d
H. Joe Lee <hyoklee@hdfgroup.org>
2024-10-19 14:39:35 -0500

    ci(poe): cd full path

--- bin/c.bat
+++ bin/c.bat
@@ -1,7 +1,7 @@
 set PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;%PATH%
 set SETVARS="c:\Program Files (x86)\Intel\oneAPI\2024.2\oneapi-vars.bat"
 call %SETVARS%
-cd build
+cd c:\Users\JoeLee\source\repos\hdf5.HDFGroup\build
 ctest  -D Continuous
 
 
