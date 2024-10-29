d0ee3eece91aa834a197d8ef5c1e2fa02e59f512
H. Joe Lee <hyoklee@hdfgroup.org>
2024-10-29 13:53:54 -0500

    ci(win10): enable thread-safe

--- bin/f.r.bat
+++ bin/f.r.bat
@@ -7,13 +7,12 @@ set LIB=D:\one\compiler\2024.2\lib;D:\vs\vc\Tools\MSVC\14.41.34120\lib\onecore\x
 set LIBPATH=D:\one\mpi\latest\lib;D:\one\compiler\2024.2\lib;D:\vs\vc\Tools\MSVC\14.41.34120\lib\onecore\x64;%LIBPATH%
 set SETVARS="D:\one\setvars.bat"
 call %SETVARS%
-REM rmdir /q /s build
-REM mkdir build
-REM cd build
 set PATH=D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\;D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;D:\VS\VC\Tools\MSVC\14.41.34120\bin\Hostx64\x64;%PATH%
-cd d:\hdf5.HDFGroup\build
-d:\cmake\bin\cmake -G Ninja -DCMAKE_C_COMPILER="D:/one/compiler/latest/bin/icx.exe" -DCMAKE_CXX_COMPILER="D:/one/compiler/latest/bin/icx.exe" -DCMAKE_Fortran_COMPILER="D:/one/compiler/2024.2/bin/ifx.exe" -DBUILD_SHARED_LIBS:BOOL=ON  -DHDF5_BUILD_FORTRAN:BOOL=ON  -DHDF5_ENABLE_PARALLEL:BOOL=OFF  -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SUBFILING_VFD:BOOL=OFF -D CTEST_DROP_SITE_INIT:STRING="my.cdash.org" -DSITE=win-10 -DBUILDNAME="ninja/icx/f" ..
+cd d:\hdf5.HDFGroup
+rmdir /q /s build
+mkdir build
+cd build
+d:\cmake\bin\cmake -G Ninja -DCMAKE_C_COMPILER="D:/one/compiler/latest/bin/icx.exe" -DCMAKE_CXX_COMPILER="D:/one/compiler/latest/bin/icx.exe" -DCMAKE_Fortran_COMPILER="D:/one/compiler/2024.2/bin/ifx.exe" -DBUILD_SHARED_LIBS:BOOL=ON  -DHDF5_BUILD_FORTRAN:BOOL=ON  -DHDF5_BUILD_HL_LIB:BOOL=ON  -DHDF5_ENABLE_PARALLEL:BOOL=OFF  -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF -DHDF5_ENABLE_THREADSAFE:BOOL=ON -DHDF5_ENABLE_SUBFILING_VFD:BOOL=OFF -D CTEST_DROP_SITE_INIT:STRING="my.cdash.org" -DSITE=win-10 -DBUILDNAME="ninja/icx/f/h/ts" ..
 REM ctest  -D Continuous
 ctest  -D Experimental
-REM cd ..
-
+cd ..
