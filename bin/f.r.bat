c3b682a2d8f9e7187bde0ea9f60271b3f5d82d57
H. Joe Lee <hyoklee@hdfgroup.org>
2024-10-31 12:10:25 -0500

    ci(oneapi): use 2025.0

--- bin/f.r.bat
+++ bin/f.r.bat
@@ -3,16 +3,16 @@ set VS2022INSTALLDIR=D:\VS\
 set SETVARSVC="D:\one\setvars-vcvarsall"
 call %SETVARSVC%
 set INCLUDE=D:\VS\VC\Tools\MSVC\14.41.34120\include;%INCLUDE%
-set LIB=D:\one\compiler\2024.2\lib;D:\vs\vc\Tools\MSVC\14.41.34120\lib\onecore\x64;%LIB%
-set LIBPATH=D:\one\mpi\latest\lib;D:\one\compiler\2024.2\lib;D:\vs\vc\Tools\MSVC\14.41.34120\lib\onecore\x64;%LIBPATH%
+set LIB=D:\one\compiler\2025.0\lib;D:\vs\vc\Tools\MSVC\14.41.34120\lib\onecore\x64;%LIB%
+set LIBPATH=D:\one\mpi\latest\lib;D:\one\compiler\2025.0\lib;D:\vs\vc\Tools\MSVC\14.41.34120\lib\onecore\x64;%LIBPATH%
 set SETVARS="D:\one\setvars.bat"
 call %SETVARS%
 set PATH=D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\;D:\VS\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;D:\VS\VC\Tools\MSVC\14.41.34120\bin\Hostx64\x64;%PATH%
 cd d:\hdf5.HDFGroup
-rmdir /q /s build
-mkdir build
+REM rmdir /q /s build
+REM mkdir build
 cd build
-d:\cmake\bin\cmake -G Ninja -DCMAKE_C_COMPILER="D:/one/compiler/latest/bin/icx.exe" -DCMAKE_CXX_COMPILER="D:/one/compiler/latest/bin/icx.exe" -DCMAKE_Fortran_COMPILER="D:/one/compiler/2024.2/bin/ifx.exe" -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_STATIC_LIBS:BOOL=ON -DHDF5_BUILD_FORTRAN:BOOL=ON  -DHDF5_BUILD_HL_LIB:BOOL=ON  -DHDF5_ENABLE_PARALLEL:BOOL=OFF  -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SUBFILING_VFD:BOOL=OFF -D CTEST_DROP_SITE_INIT:STRING="my.cdash.org" -DSITE=win-10 -DBUILDNAME="ninja/icx/+st/f/h/-sz-zl/-p" ..
-REM ctest  -D Continuous
-ctest  -D Experimental
+d:\cmake\bin\cmake -G Ninja -DCMAKE_C_COMPILER="D:/one/compiler/latest/bin/icx.exe" -DCMAKE_CXX_COMPILER="D:/one/compiler/latest/bin/icx.exe" -DCMAKE_Fortran_COMPILER="D:/one/compiler/2025.0/bin/ifx.exe" -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_STATIC_LIBS:BOOL=ON -DHDF5_BUILD_FORTRAN:BOOL=ON  -DHDF5_BUILD_HL_LIB:BOOL=ON  -DHDF5_ENABLE_PARALLEL:BOOL=OFF  -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SUBFILING_VFD:BOOL=OFF -D CTEST_DROP_SITE_INIT:STRING="my.cdash.org" -DSITE=win-10 -DBUILDNAME="ninja/icx/+st/f/h/-sz-zl/-p" ..
+ctest  -D Continuous
+REM ctest  -D Experimental
 cd ..
