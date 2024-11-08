6faf3df4a0b11b09520cf9bef72390f00994851e
H. Joe Lee <hyoklee@hdfgroup.org>
2024-11-08 09:32:36 -0600

    ci(win): start job at 11pm

--- bin/sch.bat
+++ bin/sch.bat
@@ -1,1 +1,3 @@
-schtasks /create /sc daily /st 17:08 /tn h5 /tr d:\h5p\bin\f.r.bat 
+schtasks /create /sc daily /st 23:00 /tn h5 /tr d:\h5p\bin\f.r.bat 
+REM schtasks /delete /tn h5 /f
+REM schtasks /query /fo list
