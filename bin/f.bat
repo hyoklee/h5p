60a4b6ab90b27abbdbc4b20474c675990e780822
H. Joe Lee <hyoklee@hdfgroup.org>
2025-07-07 20:43:17 -0500

    fix: use oneapi 2025.2

--- bin/f.bat
+++ bin/f.bat
@@ -1,12 +1,12 @@
 set PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;%PATH%
-set SETVARS="c:\Program Files (x86)\Intel\oneAPI\2025.0\oneapi-vars.bat"
+set SETVARS="c:\Program Files (x86)\Intel\oneAPI\2025.2\oneapi-vars.bat"
 call %SETVARS%
-copy /Y c:\Users\JoeLee\source\repos\hdf5\src\CMakeLists.txt src\
-copy /Y c:\Users\JoeLee\source\repos\hdf5\CMakeFilters.cmake .
+copy /Y c:\src\hdf5\src\CMakeLists.txt src\
+copy /Y c:\src\hdf5\CMakeFilters.cmake .
 rmdir /q /s build
 mkdir build
 cd build
-cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=C:/Users/JoeLee/source/repos/vcpkg.microsoft/scripts/buildsystems/vcpkg.cmake -DCMAKE_C_COMPILER="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/bin/icx.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/bin/icx.exe" -DBUILD_SHARED_LIBS:BOOL=OFF  -DHDF5_BUILD_FORTRAN:BOOL=ON  -DHDF5_ENABLE_PARALLEL:BOOL=ON  -DHDF5_ENABLE_ZLIB_SUPPORT:BOOL=ON -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=ON -DHDF5_ENABLE_SUBFILING_VFD:BOOL=OFF -DHDF5_TEST_API=ON -DMPIEXEC_EXECUTABLE:FILEPATH="C:/Program Files (x86)/Intel/oneAPI/mpi/latest/bin/mpiexec.exe" -DMPI_C_HEADER_DIR:PATH="C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include" -DCTEST_DROP_SITE_INIT:STRING="my.cdash.org"  -DSITE="win-11" -DBUILDNAME="ninja/icx/f/+ae+zl/p" ..
+cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=C:/src/vcpkg.microsoft/scripts/buildsystems/vcpkg.cmake -DCMAKE_C_COMPILER="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/bin/icx.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/bin/icx.exe" -DBUILD_SHARED_LIBS:BOOL=OFF  -DHDF5_BUILD_FORTRAN:BOOL=ON  -DHDF5_ENABLE_PARALLEL:BOOL=ON  -DHDF5_ENABLE_ZLIB_SUPPORT:BOOL=ON -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=ON -DHDF5_ENABLE_SUBFILING_VFD:BOOL=OFF -DHDF5_TEST_API=ON -DMPIEXEC_EXECUTABLE:FILEPATH="C:/Program Files (x86)/Intel/oneAPI/mpi/latest/bin/mpiexec.exe" -DMPI_C_HEADER_DIR:PATH="C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include" -DCTEST_DROP_SITE_INIT:STRING="my.cdash.org"  -DSITE="win-11" -DBUILDNAME="ninja/icx/f/+ae+zl/p" ..
 ctest -D Experimental
 cd ..
 
