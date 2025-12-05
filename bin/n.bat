896ee0247cc7acbd88c1ea9af3bf95b08516596d
H. Joe Lee <hyoklee@hdfgroup.org>
2025-12-05 09:38:32 -0600

    chore(raven): standardize build name

--- bin/n.bat
+++ bin/n.bat
@@ -1,7 +1,8 @@
-call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
+set SETVARS="C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
+call %SETVARS%
 rmdir /q /s n
 mkdir n
 cd n
-cmake -G Ninja -DBUILD_SHARED_LIBS:BOOL=OFF -DHDF5_ENABLE_ZLIB_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF  -DCTEST_DROP_SITE_INIT:STRING="my.cdash.org"  -DSITE="win-11" -DBUILDNAME="n/vs-26.c/-sh" ..
+cmake -G Ninja -DBUILD_SHARED_LIBS:BOOL=OFF -DHDF5_ENABLE_ZLIB_SUPPORT:BOOL=OFF -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF  -DCTEST_DROP_SITE_INIT:STRING="my.cdash.org"  -DSITE="win-11" -DBUILDNAME="pr6052/ninja/vs-26.c/-sh" ..
 ctest -D Experimental -j
 cd ..
