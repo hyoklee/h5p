9f0a9ea4461e7a5da07595e0deaa58e83b4308ee
H. Joe Lee <hyoklee@hdfgroup.org>
2026-05-15 15:42:20 -0500

    test(win,vs26c): zlib-ng

--- bin/n_arm64.bat
+++ bin/n_arm64.bat
@@ -3,6 +3,6 @@ call %SETVARS%
 rmdir /q /s n
 mkdir n
 cd n
-cmake -G Ninja -A ARM -DBUILD_SHARED_LIBS:BOOL=OFF -DHDF5_ENABLE_ZLIB_SUPPORT:BOOL=OFF  -DCMAKE_C_FLAGS="/fsanitize=address" -DCMAKE_CXX_FLAGS="/fsanitize=address" -DCTEST_DROP_SITE_INIT:STRING="my.cdash.org" -DSITE="poe" -DBUILDNAME="arm/win-11/vs-26.c/-sh/sz/asan" -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=ON -DCMAKE_TOOLCHAIN_FILE="C:\vcpkg.ms\scripts\buildsystems\vcpkg.cmake" ..
+cmake -G "Visual Studio 18 2026" -A ARM64 -DBUILD_SHARED_LIBS:BOOL=OFF -DHDF5_ENABLE_ZLIB_SUPPORT:BOOL=OFF  -DCMAKE_C_FLAGS="/fsanitize=address" -DCMAKE_CXX_FLAGS="/fsanitize=address" -DCTEST_DROP_SITE_INIT:STRING="my.cdash.org" -DSITE="poe" -DBUILDNAME="arm/win-11/vs-26.c/-sh/sz/asan" -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=ON -DCMAKE_TOOLCHAIN_FILE="C:\vcpkg.ms\scripts\buildsystems\vcpkg.cmake" ..
 ctest -D Experimental -j
 cd ..
